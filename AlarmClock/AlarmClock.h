d230f5f916e47e7fac5ae55cc8a1be1e67beefd2
OsvaldoComCH <eduhrsilva@gmail.com>
2024-10-03 09:59:10 -0300

    AlarmClockP functions created

--- AlarmClock/AlarmClock.h
+++ AlarmClock/AlarmClock.h
@@ -3,14 +3,81 @@
 
 #include <windows.h>
 
-int __fastcall AlarmClockUP(unsigned int Microseconds)
+int __FREQ_TO_US__ = 10;
+int __FREQ_TO_MS__ = 10000;
+
+int AlarmClockInit()
 {
-    QueryPerformanceFrequency
+    LARGE_INTEGER Freq;
+    if(QueryPerformanceFrequency(&Freq))
+    {
+        __FREQ_TO_US__ = Freq.QuadPart / 1000000;
+        __FREQ_TO_MS__ = Freq.QuadPart / 1000;
+        return 0;
+    }
+    __FREQ_TO_US__ = 0;
+    __FREQ_TO_MS__ = 0;
+    return 1;
 }
 
-int __fastcall AlarmClockMP(unsigned int Milliseconds)
+void AlarmClockPM(int Milliseconds)
 {
+    if(__FREQ_TO_MS__)
+    {
+        LARGE_INTEGER Time;
+        QueryPerformanceCounter(&Time);
+        long long DueTime = Milliseconds * __FREQ_TO_MS__ + Time.QuadPart;
+        long long TimeLeft = DueTime;
+        while(TimeLeft > 0)
+        {
+            if(TimeLeft > __FREQ_TO_MS__ * 17)
+            {
+                Sleep(1);
+            }else
+            if(TimeLeft > __FREQ_TO_MS__ * 2)
+            {
+                timeBeginPeriod(1);
+                Sleep(1);
+                timeEndPeriod(1);
+            }
+            QueryPerformanceCounter(&Time);
+            TimeLeft = DueTime - Time.QuadPart;
+        }
+    }else
+    {
+        timeBeginPeriod(1);
+        Sleep(Milliseconds);
+        timeEndPeriod(1);
+    }
+}
 
+void AlarmClockP(int Microseconds)
+{
+    if(__FREQ_TO_US__)
+    {
+        LARGE_INTEGER Time;
+        QueryPerformanceCounter(&Time);
+        long long DueTime = Microseconds * __FREQ_TO_US__ + Time.QuadPart;
+        long long TimeLeft = DueTime;
+        while(TimeLeft > 0)
+        {
+            if(TimeLeft > __FREQ_TO_US__ * 17000)
+            {
+                Sleep(1);
+            }else
+            if(TimeLeft > __FREQ_TO_US__ * 2000)
+            {
+                timeBeginPeriod(1);
+                Sleep(1);
+                timeEndPeriod(1);
+            }
+            QueryPerformanceCounter(&Time);
+            TimeLeft = DueTime - Time.QuadPart;
+        }
+    }else
+    {
+        AlarmClockPM(Microseconds / 1000);
+    }
 }
 
 #endif
