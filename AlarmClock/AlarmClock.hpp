f5ff8e1bdc1a4dace232e707aee41b92ab130123
OsvaldoComCH <eduhrsilva@gmail.com>
2024-10-25 11:44:34 -0300

    Removed AlarmClockP, added check to prevent negative DueTime

--- AlarmClock/AlarmClock.hpp
+++ AlarmClock/AlarmClock.hpp
@@ -2,22 +2,29 @@
 #define ALARMCLOCK
 
 #include <windows.h>
-
 #include <chrono>
-class AlarmClockC
+
+class AlarmClock
 {
     private:
-    long Error;
+    unsigned Error;
 
     public:
-    AlarmClockC()
+    AlarmClock()
     {
         Error = 0;
     }
 
-    void __fastcall SleepUs(int Microseconds)
+    void __fastcall SleepUs(unsigned Microseconds)
     {
         using namespace std::chrono;
+
+        if(Error >= Microseconds)
+        {
+            Error -= Microseconds;
+            return;
+        }
+
         time_point<high_resolution_clock, microseconds> DueTime =
         time_point_cast<microseconds>(high_resolution_clock::now() + (microseconds)(Microseconds - Error));
 
@@ -45,24 +52,5 @@ class AlarmClockC
         Error = 0;
     }
 };
-
-void AlarmClockP(int Microseconds)
-{
-    using namespace std::chrono;
-    time_point<high_resolution_clock, microseconds> DueTime =
-    time_point_cast<microseconds>(high_resolution_clock::now() + (microseconds)Microseconds);
-
-    if(Microseconds > 2000)
-    {
-        timeBeginPeriod(1);
-        Sleep((Microseconds / 1000) - 1);
-        timeEndPeriod(1);
-    }
-
-    while(DueTime > time_point_cast<microseconds>(high_resolution_clock::now()))
-    {
-        continue;
-    }
-}
 
 #endif
