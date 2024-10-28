24b47151cea6f57c73600347bbd627efb7e31690
OsvaldoComCH <eduhrsilva@gmail.com>
2024-10-28 14:50:19 -0300

    More pointer manipulation

--- Headers/CppCode.cpp
+++ Headers/CppCode.cpp
@@ -1,17 +1,68 @@
 //We program mostly in english, but sometimes in C++
 #include <iostream>
+#include <cstring>
 
-static inline unsigned short HiWord(unsigned int Long)
+typedef struct 
 {
-    return *((unsigned short *) &Long + 1);
+    int x;
+    int y;
+} StructA;
+
+typedef struct 
+{
+    int a;
+    int b;
+    char c[10];
+} StructB;
+
+int Sum(StructA * A)
+{
+    return A->x + A->y;
 }
 
-static inline unsigned short LoWord(unsigned int Long)
+//Get absolute value of integer
+#define absi(i) ((((int) i) ^ ((((int) i) >> 31))) - ((((int) i) >> 31)))
+
+
+int GetValue()
 {
-    return *((unsigned short *) &Long);
+    return 1;
+}
+
+int GetValue1()
+{
+    return 1;
+}
+
+int GetValue2()
+{
+    return 2;
 }
 
 int main()
 {
-    std::cout << HiWord(0x8000ffff) << "\n" << LoWord(0x8000ffff);
+    StructA A;
+    A.x = 0x8000ffff;
+
+    //High Word
+    std::cout << *((unsigned short *) &A.x + 1) << "\n";
+    //Low Word
+    std::cout << *((unsigned short *) &A.x) << "\n";
+
+    float F = 10;
+    //Flip sign bit of float
+    *(int *) &F = *((int *) &F) ^ 0x80000000;
+    std::cout << F << "\n";
+
+    StructB B;
+    B.a = 10;
+    B.b = 5;
+    std::strcpy(B.c, "Bom dia");
+
+    //Convert pointer type of different structs
+    std::cout << Sum((StructA *) &B) << "\n";
+
+    std::cout << absi(-60) << "\n";
+
+    std::cout << absi(40) << "\n";
 }
