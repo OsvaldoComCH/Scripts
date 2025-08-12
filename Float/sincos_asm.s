d64654a82bbe132bcac501d141c0a9843fb744ec
Eduardo Henrique Ribeiro da Silva <eduhrsilva@gmail.com>
2025-08-12 17:24:06 -0300

    .

--- Float/sincos_asm.s
+++ Float/sincos_asm.s
@@ -5,20 +5,26 @@ _Z6sincosdP13sincos_result:
     movq    $0x401921FB54442D18, %rax
     movq    %rax, %xmm1
     comisd  %xmm1, %xmm0
-    jl      .L0
+    ja      .L0
     movq    $0xC01921FB54442D18, %rax
-    movq    %rax, %xmm1
-    comisd  %xmm1, %xmm0
-    jg      .L0
+    movq    %rax, %xmm2
+    comisd  %xmm2, %xmm0
+    jb      .L0
     xorq    %rax, %rax
     jmp     .L1
 .L0:
-    movq    $1, %rax
+    movsd   %xmm0, %xmm2
+    divsd   %xmm1, %xmm2
+    cvttsd2si   %xmm2, %rax
+    cvtsi2sd    %rax, %xmm2
+    mulsd   %xmm1, %xmm2
+    subsd   %xmm2, %xmm0
 .L1:
+    movq    %xmm0, %rax
     ret
 
 _main:
-    movq    $0xC02921FB54442D18, %rax
+    movq    $0x40198861baaa937e, %rax
     movq    %rax, %xmm0
     xorq    %rcx, %rcx
     call    _Z6sincosdP13sincos_result
